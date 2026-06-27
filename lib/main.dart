2e5850680f0385f4671d42a08a19987081228235
Ahmed Makled <ahmed1.makled1@gmail.com>
2026-06-28 00:27:16 +0300

    handel errors from quizes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'core/utils/service_locator.dart' as di;
 
 import 'features/auth/presentation/cubit/auth_cubit.dart';
 import 'features/auth/presentation/ui/screens/login_screen.dart';
-// import 'features/quiz/presentation/routes/quiz_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -46,7 +45,6 @@ class MainApp extends StatelessWidget {
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'Learning Management System',
-            //routes: QuizRoutes.getRoutes(),
             home: const LoginScreen(),
           ),
         );
